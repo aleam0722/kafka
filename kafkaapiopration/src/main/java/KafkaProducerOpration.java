8401d4ea592297af8fdca610a3d3591b9db7fbb9
aleam0722 <cxb444555@outlook.com>
2020-01-02 16:11:23 +0800

    同步文件

---
+++ kafkaapiopration/src/main/java/KafkaProducerOpration.java
@@ -0,0 +1,23 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class KafkaProducerOpration  {
+    private Properties properties = new Properties();
+    @Test
+    public void myTest() throws IOException {
+        properties.load(KafkaProducerOpration.class.getClassLoader().getResourceAsStream("producer.properties"));
+        Producer<Integer,String> producer = new KafkaProducer<Integer, String>(properties);
+        System.out.println(producer);
+
+        ProducerRecord<Integer, String> record = new ProducerRecord<Integer, String>("hadoop", 10, "diaomao");
+
+        producer.send(record);
+        producer.close();
+    }
+
+}
