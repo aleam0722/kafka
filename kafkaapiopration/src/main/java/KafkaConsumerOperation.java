36ae431dccc5ae53b94d2e24d455b4a70d7e99cb
aleam0722 <cxb444555@outlook.com>
2020-01-02 21:31:04 +0800

    实现consumer消费消息

--- kafkaapiopration/src/main/java/KafkaConsumerOperation.java
+++ kafkaapiopration/src/main/java/KafkaConsumerOperation.java
@@ -1,8 +1,26 @@
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.junit.Test;
 
-public class KafkaConsumerOperation {
-    @Test
-    public void consumerTest(){
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
 
+public class KafkaConsumerOperation {
+    private Properties properties = new Properties();
+    @Test
+    public void consumerTest() throws IOException {
+        properties.load(KafkaConsumerOperation.class.getClassLoader().getResourceAsStream("consumer.properties"));
+        Consumer consumer = new KafkaConsumer(properties);
+        consumer.subscribe(Arrays.asList("hadoop"));
+        while (true) {
+            ConsumerRecords<Integer,String>  records = consumer.poll(1000);
+            for (ConsumerRecord<Integer,String> record : records) {
+                String message = record.value();
+                System.out.println(message);
+            }
+        }
     }
 }
--- kafkaapiopration/src/main/java/KafkaProducerOpration.java
+++ kafkaapiopration/src/main/java/KafkaProducerOpration.java
@@ -14,7 +14,7 @@ public class KafkaProducerOpration  {
         Producer<Integer,String> producer = new KafkaProducer<Integer, String>(properties);
         System.out.println(producer);
 
-        ProducerRecord<Integer, String> record = new ProducerRecord<Integer, String>("hadoop", 10, "nihao");
+        ProducerRecord<Integer, String> record = new ProducerRecord<Integer, String>("hadoop", 10, "xixi");
 
         producer.send(record);
         producer.close();
